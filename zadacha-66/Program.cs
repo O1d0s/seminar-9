78f3bc985f2a09f5a59d0b1d7c4e161f6a14287a
Aidos <ajdos0735@gmaul.com>
2022-12-18 00:27:34 +0600

    Сделал 66 задачу

---
+++ zadacha-66/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write(Sum(n,m));
+
+int Sum(int n, int m){
+    if(n <= m){
+        return n + Sum(n+1,m);
+    }
+    else{
+        return 0;
+    }
+}
