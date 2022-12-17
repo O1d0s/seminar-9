9375b0eb03865315d5fbe872d86960352915e872
Aidos <ajdos0735@gmaul.com>
2022-12-18 01:10:02 +0600

    Сделал 68 задачу

---
+++ zadacha-68/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число N: ");
+uint n = uint.Parse(Console.ReadLine());
+
+Console.Write("Введите число M: ");
+uint m = uint.Parse(Console.ReadLine());
+
+if(n>=0 && m>=0){
+    Console.Write(Ackerman(n,m));
+}
+else{
+    Console.Write("Числа N и M должены быть больше -1");
+}
+
+uint Ackerman(uint n, uint m){
+    if(n == 0){
+        return m+1;
+    }
+    else{
+        if(m == 0){
+            return Ackerman(n-1 , 1);
+        }
+        else{
+            return Ackerman(n-1, Ackerman(n,m-1));
+        }
+    }
+}
+
