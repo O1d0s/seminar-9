fba017f57cbc95fcac6d9667eaafedda2c40dc03
Aidos <ajdos0735@gmaul.com>
2022-12-18 00:12:29 +0600

    Сделал 64 задачу

---
+++ zadacha-64/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write(Print(n));
+
+String Print(int n){
+    if (n>0){
+        return $"{n} " +Print(n-1);
+    }
+    else{
+        return String.Empty;
+    }
+}
+
